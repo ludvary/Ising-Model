9e1de5191a90c221eae22783d60710aa59e3667c
ludvary <rskrishnadasnair@gmail.com>
2023-06-11 16:29:42 +0530

    fix typo

--- Ising_full.jl
+++ Ising_full.jl
@@ -8,10 +8,10 @@ function main()
 
     # starting from a random lattice config
     LATTICE = rand([1, -1], L, L)
-    
+
     # finding the number of loops over temperature
-    T_iters = trunc(Int64, (T_final - T_init)/T_step)
-    
+    T_iters = trunc(Int64, (T_final - T_init) / T_step)
+
     # initializing the arrays for temperature, Average Energy per spin and Average Magnetization per spin
     AVG_E = zeros(Float64, T_iters)
     AVG_M = zeros(Float64, T_iters)
@@ -19,7 +19,7 @@ function main()
 
     # this finds Magnetization of the lattice (not magnetization per spin, just the magnetization)
     function find_magnetization(lattice)
-       return sum(lattice) 
+        return sum(lattice)
     end
 
 
@@ -27,10 +27,10 @@ function main()
     function find_energy(lattice)
         E = 0
         for v in 1:L
-		    for b in 1:L
-	    	    E -= J * (lattice[v, b] * (lattice[v, mod1(b+1, L)] + lattice[v, mod1(b-1, L)] + lattice[mod1(v+1, L), b] + lattice[mod1(v-1, L), b]))
-    		end
-    	end
+            for b in 1:L
+                E -= J * (lattice[v, b] * (lattice[v, mod1(b + 1, L)] + lattice[v, mod1(b - 1, L)] + lattice[mod1(v + 1, L), b] + lattice[mod1(v - 1, L), b]))
+            end
+        end
         return E
     end
 
@@ -41,27 +41,27 @@ function main()
         for t in 1:mcs_steps
             p, q = rand(1:L), rand(1:L) # choosing a random lattice point
 
-            del_E = 2* J * (lattice[p, q] * (lattice[p, mod1(q+1, L)] + lattice[p, mod1(q-1, L)] + lattice[mod1(p+1, L), q] + lattice[mod1(p-1, L), q])) # finding the energy change before and after the randomly choosen spin is flipped.
+            del_E = 2 * J * (lattice[p, q] * (lattice[p, mod1(q + 1, L)] + lattice[p, mod1(q - 1, L)] + lattice[mod1(p + 1, L), q] + lattice[mod1(p - 1, L), q])) # finding the energy change before and after the randomly choosen spin is flipped.
 
             # now we employ Metropolis-Hastings
             if del_E < 0
                 lattice[p, q] = -lattice[p, q] # spin flip is accepted
                 E += del_E                     # instantaneous values of E and M are calculated
-                M += 2*lattice[p, q]           # we could've just called find_energy() and find_magnetization() here but that would've been overhead
-            
+                M += 2 * lattice[p, q]         # we could've just called find_energy() and find_magnetization() here but that would've been overhead
+
             else
-                prob = exp(-del_E/Temparature)
+                prob = exp(-del_E / Temparature)
                 random = rand()
-                
+
                 if random < prob
                     lattice[p, q] = -lattice[p, q] # spin flip is accepted
                     E += del_E                     # instantaneous values of E and M are calculated
-                    M += 2*lattice[p, q]
+                    M += 2 * lattice[p, q]
                 end
             end
         end
 
-       return lattice # we don't want to collect any data when this function is called, hence it only returns the lattice which has been equilibrated
+        return lattice # we don't want to collect any data when this function is called, hence it only returns the lattice which is at equillibrium 
     end
 
 
@@ -76,26 +76,26 @@ function main()
         for t in 1:sim_steps
             p, q = rand(1:L), rand(1:L) # choosing random lattice point
 
-            del_E = 2*J * (lattice[p, q] * (lattice[p, mod1(q+1, L)] + lattice[p, mod1(q-1, L)] + lattice[mod1(p+1, L), q] + lattice[mod1(p-1, L), q]))# finding the energy change before and after the randomly choosen spin is flipped. (we could've done this by finding E_initial and flipping the spin and finding E_final and then subtracting both but this way is computationally cheaper)
+            del_E = 2 * J * (lattice[p, q] * (lattice[p, mod1(q + 1, L)] + lattice[p, mod1(q - 1, L)] + lattice[mod1(p + 1, L), q] + lattice[mod1(p - 1, L), q]))# finding the energy change before and after the randomly choosen spin is flipped. (we could've done this by finding E_initial and flipping the spin and finding E_final and then subtracting both but this way is computationally cheaper)
 
             # apply Metropolis-Hastings
             if del_E < 0
                 lattice[p, q] = -lattice[p, q] # spin flip is accepted
                 E += del_E                     # finding instantaneous energy and magnetization
-                M += 2*lattice[p, q]
-            
+                M += 2 * lattice[p, q]
+
             else
-                prob = exp(-del_E/T)
+                prob = exp(-del_E / T)
                 random = rand()
-                
+
                 if random < prob
                     lattice[p, q] = -lattice[p, q] # spin flip is accepted
                     E += del_E                     # finding the instantaneous energy and magnetization
-                    M += 2*lattice[p, q]
+                    M += 2 * lattice[p, q]
                 end
             end
-            E_arr[t] = E/(L*L) # storing the values of E and M inside arrays. the averages of these arrays will be calculated and stored each time do_simlulation() is called and then the arrays will be reintialized
-            M_arr[t] = M/(L*L)
+            E_arr[t] = E / (L * L) # storing the values of E and M inside arrays. the averages of these arrays will be calculated and stored each time do_simlulation() is called and then the arrays will be reintialized
+            M_arr[t] = M / (L * L)
         end
         return E_arr, M_arr
     end
@@ -103,18 +103,18 @@ function main()
 
 
     function find_variance(array)
-        return var(array) 
+        return var(array)
     end
-    
 
 
-    for i in 1:T_iters 
-        T = T_init + i*T_step # updating T
+
+    for i in 1:T_iters
+        T = T_init + i * T_step # updating T
         LATTICE = do_equillibrium(LATTICE, T) # bringing the lattice at equilibrium at T and storing the lattice config in LATTICE
         E_arr, M_arr = do_simlulation(LATTICE, T) # input that equilibrated lattice and collect data
 
-        AVG_E[i] = find_variance(E_arr/(T^2)) # store it here for a particular T
-        AVG_M[i] = find_variance(M_arr/T)
+        AVG_E[i] = find_variance(E_arr / (T^2)) # store it here for a particular T
+        AVG_M[i] = find_variance(M_arr / T)
         T_ARR[i] = T
     end # repeat for all T
 
